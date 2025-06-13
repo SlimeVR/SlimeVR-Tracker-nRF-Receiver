4554c4e1bbbabe6dabc7fe5db7c9fbb0b1fdffe9
sctanf <36978460+sctanf@users.noreply.github.com>
2025-06-13 14:55:49 -0500

    always use max output power for acknowledgement
    for fem

--- src/connection/esb.c
+++ src/connection/esb.c
@@ -164,7 +164,7 @@ int esb_initialize(bool tx)
 		config.event_handler = event_handler;
 		// config.bitrate = ESB_BITRATE_2MBPS;
 		// config.crc = ESB_CRC_16BIT;
-		config.tx_output_power = 8;
+		config.tx_output_power = 30;
 		// config.retransmit_delay = 600;
 		config.retransmit_count = 0;
 		config.tx_mode = ESB_TXMODE_MANUAL;
@@ -179,7 +179,7 @@ int esb_initialize(bool tx)
 		config.event_handler = event_handler;
 		// config.bitrate = ESB_BITRATE_2MBPS;
 		// config.crc = ESB_CRC_16BIT;
-		config.tx_output_power = 8;
+		config.tx_output_power = 30;
 		// config.retransmit_delay = 600;
 		// config.retransmit_count = 3;
 		// config.tx_mode = ESB_TXMODE_AUTO;
